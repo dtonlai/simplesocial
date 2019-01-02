3a5fcb59b79403e3629352dcc5a6eb5146fdbbb4
David Ton-Lai <dtonlai@ualberta.ca>
2019-01-02 15:53:32 -0700

    Created group app views, and setup html templates

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls imort reverse
+from django.views import generic
+from groups.models import Group,GroupMember
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequired,Mixin)
 
-# Create your views here.
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields=('name','description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
